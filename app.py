fd21c56546be8b86d20c2464544d96137063051a
Ketan Gangwal <ketan.gangwal20@vit.edu>
2022-06-30 00:01:20 +0530

    ketan

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+from email.errors import MalformedHeaderDefect
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
